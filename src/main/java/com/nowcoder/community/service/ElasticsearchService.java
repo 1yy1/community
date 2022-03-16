b3e467c4e376a301f13f25ec5d0bb4bfb2bfcef4
YYY <1398948224@qq.com>
2022-03-16 17:46:02 +0800

    2022-3-16

--- src/main/java/com/nowcoder/community/service/ElasticsearchService.java
+++ src/main/java/com/nowcoder/community/service/ElasticsearchService.java
@@ -28,64 +28,76 @@ import java.util.List;
 
 @Service
 public class ElasticsearchService {
-    @Autowired
-    private DiscussPostRepository discussPostRepository;
-    @Autowired
-    private ElasticsearchTemplate elasticsearchTemplate;
-    public void saveDiscussPost(DiscussPost post){
-        discussPostRepository.save(post);
-    }
-    public void deleteDiscussPost(int id){
-        discussPostRepository.deleteById(id);
-    }
-    public Page<DiscussPost> searchDiscussPost(String keyword,int current,int limit){
-        SearchQuery searchQuery=new NativeSearchQueryBuilder()
-                .withQuery(QueryBuilders.multiMatchQuery(keyword,"title","content"))
-                .withSort(SortBuilders.fieldSort("type").order(SortOrder.DESC))
-                .withSort(SortBuilders.fieldSort("score").order(SortOrder.DESC))
-                .withSort(SortBuilders.fieldSort("createTime").order(SortOrder.DESC))
-                .withPageable(PageRequest.of(current,limit))
-                .withHighlightFields(
-                        new HighlightBuilder.Field("title").preTags("<em>").postTags("</em>"),
-                        new HighlightBuilder.Field("content").preTags("<em>").postTags("</em>")
-                ).build();
-        return elasticsearchTemplate.queryForPage(searchQuery, DiscussPost.class, new SearchResultMapper() {
-            @Override
-            public <T> AggregatedPage<T> mapResults(SearchResponse response, Class<T> aClass, Pageable pageable) {
-                SearchHits hits = response.getHits();
-                if (hits.getTotalHits()<=0){
-                    return null;
-                }
-                List<DiscussPost> list=new ArrayList<>();
-                for (SearchHit hit:hits){
-                    DiscussPost post=new DiscussPost();
-                    String id = hit.getSourceAsMap().get("id").toString();
-                    post.setId(Integer.valueOf(id));
-                    String userId=hit.getSourceAsMap().get("userId").toString();
-                    post.setUserId(Integer.valueOf(userId));
-                    String title=hit.getSourceAsMap().get("title").toString();
-                    post.setTitle(title);
-                    String content=hit.getSourceAsMap().get("content").toString();
-                    post.setContent(content);
-                    String status=hit.getSourceAsMap().get("status").toString();
-                    post.setStatus(Integer.valueOf(status));
-                    String createTime=hit.getSourceAsMap().get("createTime").toString();
-                    post.setCreateTime(new Date(Long.valueOf(createTime)));
-                    String commentCount=hit.getSourceAsMap().get("commentCount").toString();
-                    post.setCommentCount(Integer.valueOf(commentCount));
-                    //处理高亮信息
-                    HighlightField titleFidld = hit.getHighlightFields().get("title");
-                    if (titleFidld!=null){
-                        post.setTitle(titleFidld.getFragments()[0].toString());
-                    }
-                    HighlightField contentFidld = hit.getHighlightFields().get("content");
-                    if (contentFidld!=null){
-                        post.setContent(contentFidld.getFragments()[0].toString());
-                    }
-                    list.add(post);
-                }
-                return new AggregatedPageImpl(list,pageable, hits.getTotalHits(),response.getAggregations(), response.getScrollId(), hits.getMaxScore());
+  @Autowired private DiscussPostRepository discussPostRepository;
+  @Autowired private ElasticsearchTemplate elasticsearchTemplate;
+
+  public void saveDiscussPost(DiscussPost post) {
+    discussPostRepository.save(post);
+  }
+
+  public void deleteDiscussPost(int id) {
+    discussPostRepository.deleteById(id);
+  }
+
+  public Page<DiscussPost> searchDiscussPost(String keyword, int current, int limit) {
+    SearchQuery searchQuery =
+        new NativeSearchQueryBuilder()
+            .withQuery(QueryBuilders.multiMatchQuery(keyword, "title", "content"))
+            .withSort(SortBuilders.fieldSort("type").order(SortOrder.DESC))
+            .withSort(SortBuilders.fieldSort("score").order(SortOrder.DESC))
+            .withSort(SortBuilders.fieldSort("createTime").order(SortOrder.DESC))
+            .withPageable(PageRequest.of(current, limit))
+            .withHighlightFields(
+                new HighlightBuilder.Field("title").preTags("<em>").postTags("</em>"),
+                new HighlightBuilder.Field("content").preTags("<em>").postTags("</em>"))
+            .build();
+    return elasticsearchTemplate.queryForPage(
+        searchQuery,
+        DiscussPost.class,
+        new SearchResultMapper() {
+          @Override
+          public <T> AggregatedPage<T> mapResults(
+              SearchResponse response, Class<T> aClass, Pageable pageable) {
+            SearchHits hits = response.getHits();
+            if (hits.getTotalHits() <= 0) {
+              return null;
             }
+            List<DiscussPost> list = new ArrayList<>();
+            for (SearchHit hit : hits) {
+              DiscussPost post = new DiscussPost();
+              String id = hit.getSourceAsMap().get("id").toString();
+              post.setId(Integer.valueOf(id));
+              String userId = hit.getSourceAsMap().get("userId").toString();
+              post.setUserId(Integer.valueOf(userId));
+              String title = hit.getSourceAsMap().get("title").toString();
+              post.setTitle(title);
+              String content = hit.getSourceAsMap().get("content").toString();
+              post.setContent(content);
+              String status = hit.getSourceAsMap().get("status").toString();
+              post.setStatus(Integer.valueOf(status));
+              String createTime = hit.getSourceAsMap().get("createTime").toString();
+              post.setCreateTime(new Date(Long.valueOf(createTime)));
+              String commentCount = hit.getSourceAsMap().get("commentCount").toString();
+              post.setCommentCount(Integer.valueOf(commentCount));
+              // 处理高亮信息
+              HighlightField titleFidld = hit.getHighlightFields().get("title");
+              if (titleFidld != null) {
+                post.setTitle(titleFidld.getFragments()[0].toString());
+              }
+              HighlightField contentFidld = hit.getHighlightFields().get("content");
+              if (contentFidld != null) {
+                post.setContent(contentFidld.getFragments()[0].toString());
+              }
+              list.add(post);
+            }
+            return new AggregatedPageImpl(
+                list,
+                pageable,
+                hits.getTotalHits(),
+                response.getAggregations(),
+                response.getScrollId(),
+                hits.getMaxScore());
+          }
         });
-    }
+  }
 }
